3697a9a468ff27e3ee28e737f06b1c380b47e4b4
Sivasundar <2212065@nec.edu.in>
2025-07-20 20:03:40 +0530

    Add files via upload

---
+++ Week 5/Creating Microservices for account and loan/AccountController.java
@@ -0,0 +1,16 @@
+package com.cognizant.account.controller;
+
+import org.springframework.web.bind.annotation.*;
+import java.util.*;
+
+@RestController
+public class AccountController{
+    @GetMapping("/accounts/{number}")
+    public Map<String, Object> getAccountDetails(@PathVariable String number){
+        Map<String, Object> account = new HashMap<>();
+        account.put("number", number);
+        account.put("type", "savings");
+        account.put("balance", 234343);
+        return account;
+    }
+}
